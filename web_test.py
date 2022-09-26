77b390628edabc5cf62ef412d6aad3b804fe5dd7
zhangzijie98 <114359709+zhangzijie98@users.noreply.github.com>
2022-09-26 12:36:00 +0800

    Update web_test.py

--- web_test.py
+++ web_test.py
@@ -5,7 +5,7 @@ from st_aggrid import AgGrid
 import pandas as pd
 import pymysql
 from sqlalchemy import create_engine
-import inspect
+#import inspect
 #import asyncio
 
 engine = create_engine('mysql+pymysql://zhang:Zhang1988@124.71.83.147:3306/wq_rs?charset=utf8')
@@ -17,7 +17,7 @@ uploaded_file = st.file_uploader("请选择要上传的xls格式表格！")
 if uploaded_file is not None:
     df1 = pd.read_excel(uploaded_file)
     AgGrid(df1.head(10))
-    df1.to_sql(name=str(uploaded_file.name).replace(".xls",""), con=engine, chunksize=1000, if_exists='replace', index=None)
+    df1.to_sql(name=str(uploaded_file.name).replace(".xls",""), con=engine, chunksize=1000, if_exists='replace', index=False)
     st.success("上传成功！")
     
     db = pymysql.connect(host="124.71.83.147", port=3306,user="zhang", password="Zhang1988", database="wq_rs", charset="utf8")
